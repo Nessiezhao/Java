e1ded1bd752c7491fc8fb186b1501a1d120c8107
Nessiezhao <Nessie_zhao@163.com>
2018-10-31 09:24:57 +0800

    add 递归版的快排

--- test/test1/test.java
+++ test/test1/test.java
@@ -4,7 +4,46 @@ public class test{
         //System.out.println(add(2,3,5));函数重载
         //System.out.println(sum(100));递归求前100个数的和
         //mul(9);//9*9乘法表
-        System.out.println(jiecheng(60));
+        //System.out.println(jiecheng(60));
+        //递归实现快排
+        int[] array = new int[]{5,6,2,4,8,1,3};
+        for(int i = 0;i < array.length;i++)
+        {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+        QuikSort(array,0,array.length-1);
+        for(int i = 0;i < array.length;i++)
+        {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+    public static int getmid(int[] arr,int left,int right){
+        int key = arr[left];
+        while(left < right)
+        {
+            while(left < right && arr[right] > key)
+            {
+                right--;
+            }
+            arr[left] = arr[right];
+            while(left < right && arr[left] < key)
+            {
+                left++;
+            }
+            arr[right] = arr[left];
+        }
+        arr[left] = key;
+        return left;
+    }
+    public static void QuikSort(int[] arr,int start,int end){
+        if(start < end)
+        {
+            int mid = getmid(arr,start,end);
+            QuikSort(arr,start,mid-1);
+            QuikSort(arr,mid+1,end);
+        }
     }
     public static double jiecheng(int num){
         if(num == 1)
